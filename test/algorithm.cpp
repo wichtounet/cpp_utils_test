b41c613344e0e33055651d2bd7ce7a2546314aa6
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-10 14:29:37 +0100

    First series of test

--- test/algorithm.cpp
+++ test/algorithm.cpp
@@ -5,10 +5,156 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
+#include <vector>
+
 #include "catch.hpp"
 
 #include "cpp_utils/algorithm.hpp"
 
 TEST_CASE( "foreach/container", "foreach_1" ) {
     std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+
+    cpp::foreach(a, [&sum](auto& v){ sum += v; });
+
+    REQUIRE(sum == 15);
+}
+
+TEST_CASE( "foreach/range", "foreach_2" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+    cpp::foreach(a.begin(), a.end(), [&sum](auto& v){ sum += v; });
+
+    REQUIRE(sum == 15);
+}
+
+TEST_CASE( "foreach/references", "foreach_3" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+    cpp::foreach(a.begin(), a.end(), [&sum](auto& v){ v += 1; });
+
+    REQUIRE(a[0] == 2);
+    REQUIRE(a[1] == 3);
+    REQUIRE(a[2] == 4);
+    REQUIRE(a[3] == 5);
+    REQUIRE(a[4] == 6);
+}
+
+TEST_CASE( "foreach_it/container", "foreach_it_1" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+
+    cpp::foreach_it(a, [&sum](auto it){ sum += *it; });
+
+    REQUIRE(sum == 15);
+}
+
+TEST_CASE( "foreach_it/range", "foreach_it_2" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+    cpp::foreach_it(a.begin(), a.end(), [&sum](auto it){ sum += *it; });
+
+    REQUIRE(sum == 15);
+}
+
+TEST_CASE( "foreach_it/references", "foreach_it_3" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+    cpp::foreach_it(a.begin(), a.end(), [&sum](auto v){ *v += 1; });
+
+    REQUIRE(a[0] == 2);
+    REQUIRE(a[1] == 3);
+    REQUIRE(a[2] == 4);
+    REQUIRE(a[3] == 5);
+    REQUIRE(a[4] == 6);
+}
+
+TEST_CASE( "foreach_i/container", "foreach_i_1" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum_1 = 0;
+    int sum_2 = 0;
+
+    cpp::foreach_i(a,
+        [&sum_1,&sum_2,&a](auto& v, std::size_t i){ REQUIRE(a[i] == v); sum_1 += v; sum_2 += a[i];});
+
+    REQUIRE(sum_1 == 15);
+    REQUIRE(sum_2 == 15);
+}
+
+TEST_CASE( "foreach_i/range", "foreach_i_2" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum_1 = 0;
+    int sum_2 = 0;
+
+    cpp::foreach_i(a.begin(), a.end(),
+        [&sum_1,&sum_2,&a](auto& v, std::size_t i){ REQUIRE(a[i] == v); sum_1 += v; sum_2 += a[i];});
+
+    REQUIRE(sum_1 == 15);
+    REQUIRE(sum_2 == 15);
+}
+
+TEST_CASE( "foreach_i/references", "foreach_it_3" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int sum = 0;
+    cpp::foreach_i(a.begin(), a.end(),
+        [&sum,&a](auto& v, std::size_t i){ v += 1; a[i] += 1; });
+
+    REQUIRE(a[0] == 3);
+    REQUIRE(a[1] == 4);
+    REQUIRE(a[2] == 5);
+    REQUIRE(a[3] == 6);
+    REQUIRE(a[4] == 7);
+}
+
+TEST_CASE( "foreach_pair/container", "foreach_pair_1" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int i = 0;
+
+    cpp::foreach_pair(a,
+        [&i](auto& v1, auto& v2){ REQUIRE(v1 != v2); ++i;});
+
+    REQUIRE(i == 10);
+}
+
+TEST_CASE( "foreach_pair/range", "foreach_pair_2" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int i = 0;
+
+    cpp::foreach_pair(a.begin(), a.end(),
+        [&i](auto& v1, auto& v2){ REQUIRE(v1 != v2); ++i;});
+
+    REQUIRE(i == 10);
+}
+
+TEST_CASE( "foreach_pair_it/container", "foreach_pair_it_1" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int i = 0;
+
+    cpp::foreach_pair_it(a,
+        [&i](auto i1, auto i2){ REQUIRE(*i1 != *i2); ++i;});
+
+    REQUIRE(i == 10);
+}
+
+TEST_CASE( "foreach_pair_it/range", "foreach_pair_it_2" ) {
+    std::vector<int> a{1,2,3,4,5};
+
+    int i = 0;
+
+    cpp::foreach_pair_it(a.begin(), a.end(),
+        [&i](auto i1, auto i2){ REQUIRE(*i1 != *i2); ++i;});
+
+    REQUIRE(i == 10);
 }
