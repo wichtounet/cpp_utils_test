0b1a4db4dd4f4ac6a5d2d0b3b98bf062b3839c65
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-11 09:12:58 +0100

    Add string trim tests

---
+++ test/string.cpp
@@ -0,0 +1,60 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <string>
+
+#include "catch.hpp"
+
+#include "cpp_utils/string.hpp"
+
+TEST_CASE( "ltrim", "ltrim" ) {
+    std::string a = "    asdf";
+    std::string b = "    asdf    ";
+    std::string c = "asdf";
+
+    REQUIRE(cpp::ltrim(a) == "asdf");
+    REQUIRE(cpp::ltrim(b) == "asdf    ");
+    REQUIRE(cpp::ltrim(c) == "asdf");
+
+    REQUIRE(a == "asdf");
+    REQUIRE(b == "asdf    ");
+    REQUIRE(c == "asdf");
+}
+
+TEST_CASE( "rtrim", "rtrim" ) {
+    std::string a = "    asdf";
+    std::string b = "    asdf    ";
+    std::string c = "asdf";
+    std::string d = "asdf         ";
+
+    REQUIRE(cpp::rtrim(a) == "    asdf");
+    REQUIRE(cpp::rtrim(b) == "    asdf");
+    REQUIRE(cpp::rtrim(c) == "asdf");
+    REQUIRE(cpp::rtrim(d) == "asdf");
+
+    REQUIRE(a == "    asdf");
+    REQUIRE(b == "    asdf");
+    REQUIRE(c == "asdf");
+    REQUIRE(d == "asdf");
+}
+
+TEST_CASE( "trim", "trim" ) {
+    std::string a = "    asdf";
+    std::string b = "    asdf    ";
+    std::string c = "asdf";
+    std::string d = "asdf         ";
+
+    REQUIRE(cpp::trim(a) == "asdf");
+    REQUIRE(cpp::trim(b) == "asdf");
+    REQUIRE(cpp::trim(c) == "asdf");
+    REQUIRE(cpp::trim(d) == "asdf");
+
+    REQUIRE(a == "asdf");
+    REQUIRE(b == "asdf");
+    REQUIRE(c == "asdf");
+    REQUIRE(d == "asdf");
+}
