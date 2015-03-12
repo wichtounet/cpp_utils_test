592ae86232bc89103687e52fa18521d4091f1380
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-03-12 08:24:21 +0100

    Test for likely.hpp

---
+++ test/likely.cpp
@@ -0,0 +1,40 @@
+//=======================================================================
+// Copyright (c) 2014-2015 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <string>
+
+#include "catch.hpp"
+
+#include "cpp_utils/likely.hpp"
+
+TEST_CASE( "likely", "likely" ) {
+    std::string a = "asdf";
+    std::string b = "asdf";
+
+    bool ok = false;
+    if(cpp_likely(a == b)){
+        ok = true;
+    } else {
+        ok = false;
+    }
+
+    REQUIRE(ok);
+}
+
+TEST_CASE( "unlikely", "unlikely" ) {
+    std::string a = "asdf";
+    std::string b = "asdf";
+
+    bool ok = false;
+    if(cpp_unlikely(a == b)){
+        ok = true;
+    } else {
+        ok = false;
+    }
+
+    REQUIRE(ok);
+}
