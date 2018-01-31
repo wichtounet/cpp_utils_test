aac421994f041bc093838a78458746dfc14482e9
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-01-31 08:39:15 +0100

    Tests for soft_aligned_array

---
+++ test/soft_aligned_array.cpp
@@ -0,0 +1,145 @@
+//=======================================================================
+// Copyright (c) 2014-2016 Baptiste Wicht
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <string>
+#include <iostream>
+
+#include "catch.hpp"
+
+#include "cpp_utils/soft_aligned_array.hpp"
+
+TEST_CASE( "soft_aligned_array/1", "[array_wrapper]" ) {
+    cpp::soft_aligned_array<float, 7, 32> array;
+
+    REQUIRE(array.size() == 7);
+    REQUIRE((reinterpret_cast<size_t>(array.data()) % 32) == 0);
+
+    for(std::size_t i = 0; i < 7; ++i){
+        array[i] = i;
+        REQUIRE(array[i] == float(i));
+    }
+
+    int i = 0;
+    for(auto& value : array){
+        REQUIRE(value == float(i));
+        ++i;
+    }
+
+    i = 0;
+    auto it = array.cbegin();
+    auto end = array.cend();
+    while(it != end){
+        REQUIRE(*it == float(i));
+        ++it;
+        ++i;
+    }
+
+    i = 6;
+    auto rit = array.crbegin();
+    auto rend = array.crend();
+    while(rit != rend){
+        CHECK(*rit == float(i));
+        ++rit;
+        --i;
+    }
+}
+
+TEST_CASE( "soft_aligned_array/2", "[array_wrapper]" ) {
+    cpp::soft_aligned_array<double, 7, 64> array;
+
+    REQUIRE(array.size() == 7);
+    REQUIRE((reinterpret_cast<size_t>(array.data()) % 64) == 0);
+
+    for(std::size_t i = 0; i < 7; ++i){
+        array[i] = i;
+        REQUIRE(array[i] == double(i));
+    }
+
+    int i = 0;
+    for(auto& value : array){
+        REQUIRE(value == double(i));
+        ++i;
+    }
+
+    i = 0;
+    auto it = array.cbegin();
+    auto end = array.cend();
+    while(it != end){
+        REQUIRE(*it == double(i));
+        ++it;
+        ++i;
+    }
+
+    i = 6;
+    auto rit = array.crbegin();
+    auto rend = array.crend();
+    while(rit != rend){
+        CHECK(*rit == double(i));
+        ++rit;
+        --i;
+    }
+}
+
+TEST_CASE( "soft_aligned_array/3", "[array_wrapper]" ) {
+    alignas(32) cpp::soft_aligned_array<float, 7, 32> array;
+
+    REQUIRE(array.size() == 7);
+    REQUIRE((reinterpret_cast<size_t>(array.data()) % 32) == 0);
+    REQUIRE((reinterpret_cast<size_t>(&array) % 32) == 0);
+    REQUIRE(reinterpret_cast<size_t>(array.data()) == reinterpret_cast<size_t>(&array));
+
+    for(std::size_t i = 0; i < 7; ++i){
+        array[i] = i;
+        REQUIRE(array[i] == float(i));
+    }
+}
+
+// Aligned dynamic allocation for over-aligned type is only guaranteed starting
+// from C++17
+
+#if __cpp_aligned_new >= 201606
+
+TEST_CASE( "soft_aligned_array/4", "[array_wrapper]" ) {
+    using type = cpp::soft_aligned_array<float, 7, 32>;
+    auto* array = new type;
+
+    REQUIRE(array->size() == 7);
+    REQUIRE((reinterpret_cast<size_t>(array->data()) % 32) == 0);
+
+    for(std::size_t i = 0; i < 7; ++i){
+        (*array)[i] = i;
+        REQUIRE((*array)[i] == float(i));
+    }
+
+    int i = 0;
+    for(auto& value : *array){
+        REQUIRE(value == float(i));
+        ++i;
+    }
+
+    i = 0;
+    auto it = array->cbegin();
+    auto end = array->cend();
+    while(it != end){
+        REQUIRE(*it == float(i));
+        ++it;
+        ++i;
+    }
+
+    i = 6;
+    auto rit = array->crbegin();
+    auto rend = array->crend();
+    while(rit != rend){
+        CHECK(*rit == float(i));
+        ++rit;
+        --i;
+    }
+
+    std::cout << "shoudl2" << std::endl;
+}
+
+#endif
