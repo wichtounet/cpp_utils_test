0e3e167fec2cca5e9c840218b2344fc6bfc4d062
Baptiste Wicht <baptiste.wicht@gmail.com>
2018-01-31 08:09:37 +0100

    New test for aligned_array

--- test/aligned_array.cpp
+++ test/aligned_array.cpp
@@ -97,3 +97,40 @@ TEST_CASE( "aligned_array/3", "[array_wrapper]" ) {
         REQUIRE(array[i] == float(i));
     }
 }
+
+TEST_CASE( "aligned_array/4", "[array_wrapper]" ) {
+    using type = cpp::aligned_array<float, 7, 32>;
+    auto* array = new type;
+
+    REQUIRE(array->size() == 7);
+    REQUIRE((reinterpret_cast<size_t>(array->data()) % 32) == 0);
+
+    for(std::size_t i = 0; i < 7; ++i){
+        (*array)[i] = i;
+        REQUIRE((*array)[i] == float(i));
+    }
+
+    int i = 0;
+    for(auto& value : *array){
+        REQUIRE(value == float(i));
+        ++i;
+    }
+
+    i = 0;
+    auto it = array->cbegin();
+    auto end = array->cend();
+    while(it != end){
+        REQUIRE(*it == float(i));
+        ++it;
+        ++i;
+    }
+
+    i = 6;
+    auto rit = array->crbegin();
+    auto rend = array->crend();
+    while(rit != rend){
+        CHECK(*rit == float(i));
+        ++rit;
+        --i;
+    }
+}
